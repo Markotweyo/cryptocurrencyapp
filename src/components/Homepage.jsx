57808c780c8a3ef07319cddcd949d66f81518c7f
Mark Otweyo <markotweyo99@gmail.com>
2021-11-04 13:12:46 +0300

    Added Fetching cryptocurrency  data to cryptocurrencies component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import millify from 'millify';
 import {Typography, Row, Col, Statistic} from 'antd';
 import { Link} from 'react-router-dom';
+import {Cryptocurrencies, News} from './index'
 
 import { useGetCryptosQuery } from '../services/cryptoApi'
 const {Title}= Typography;
@@ -9,7 +10,7 @@ const {Title}= Typography;
 const Homepage = () => {
     const { data, isFetching } = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
-    console.log(globalStats)
+    
 
     if(isFetching) return "Loading...";
 
@@ -22,9 +23,19 @@ const Homepage = () => {
                <Col span={12}> <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
                <Col span={12}> <Statistic title="Total Market Cap" value={`$${millify(globalStats.totalMarketCap)}`}/></Col>
                <Col span={12}> <Statistic title="Total 24h volume" value={`$${millify(globalStats.total24hVolume)}`}/></Col>
-               <Col span={12}> <Statistic title="Total Markets" value={ globalStats.totalMarkets }/></Col>
+               <Col span={12}> <Statistic title="Total Markets" value={ millify(globalStats.totalMarkets) }/></Col>
                <Col span={12}> <Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
            </Row>
+           <div className="home-heading-container">
+               <Title level ={2} className="home-title">Top 10 Cryptocurrencies in the World</Title>
+               <Title level ={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+           </div>
+           <Cryptocurrencies simplified/>
+           <div className="home-heading-container">
+               <Title level ={2} className="home-title">Latest Crypto News</Title>
+               <Title level ={3} className="show-more"><Link to="/news">Show More</Link></Title>
+           </div>
+           <News simplified/>
         </>
     )
 }
